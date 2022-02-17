af586d71ac84b8d7315f519da7876252636f390b
Weitung Liao <>
2022-02-16 21:23:33 -0700

    no message

--- src/main/java/com/example/mysqljdbc/grpcClient.java
+++ src/main/java/com/example/mysqljdbc/grpcClient.java
@@ -2,14 +2,13 @@ package com.example.mysqljdbc;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-
+//34.106.36.207
 public class grpcClient {
     public static void main(String[] args){
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",8050).usePlaintext().build();
         db_servicesGrpc.db_servicesBlockingStub stub=db_servicesGrpc.newBlockingStub(channel);
-        item i = item.newBuilder().setItemName("grpcTest").setQuantity(3).build();
+        item i = item.newBuilder().setItemName("grpcTest2").setQuantity(3).setSellerId(1).build();
         stub.saveItem(i);
-
         channel.shutdown();
     }
 }
--- src/main/java/com/example/mysqljdbc/itemServiceImpl.java
+++ src/main/java/com/example/mysqljdbc/itemServiceImpl.java
@@ -6,13 +6,25 @@ import java.sql.*;
 
 public class itemServiceImpl extends db_servicesGrpc.db_servicesImplBase{
     String connectionUrl = "jdbc:mysql://34.106.36.207:3306/product_db?verifyServerCertificate=false&useSSL=False";
-
+    String user="root";
+    String password="123123";
     @Override
     public void saveItem(item input_item, StreamObserver<Empty> empty){
-        String sql = "INSERT INTO product_db.items (item_name, item_category, quantity, sale_price, item_condition, keyword1, keyword2, keyword3, keyword4, keyword5, seller_id) VALUES ('grpc', 2, 3, 12, null, null, null, null, null, null, null)";
-        try (Connection conn = DriverManager.getConnection(connectionUrl, "root", "123123");
+        String sql = "INSERT INTO product_db.items (item_name, item_category, quantity, sale_price, item_condition, keyword1, keyword2, keyword3, keyword4, keyword5, seller_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)";
+        try (Connection conn = DriverManager.getConnection(connectionUrl, user, "123123");
              ) {
             PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, input_item.getItemName());
+            ps.setInt(2, input_item.getItemCategory());
+            ps.setInt(3, input_item.getQuantity());
+            ps.setDouble(4, input_item.getSalePrice());
+            ps.setString(5, input_item.getItemCondition());
+            ps.setString(6, input_item.getKeyword1());
+            ps.setString(7, input_item.getKeyword2());
+            ps.setString(8, input_item.getKeyword3());
+            ps.setString(9, input_item.getKeyword4());
+            ps.setString(10, input_item.getKeyword5());
+            ps.setInt(11, input_item.getSellerId());
             int rs = ps.executeUpdate();
         } catch (SQLException e) {
             System.out.println(e.toString());
